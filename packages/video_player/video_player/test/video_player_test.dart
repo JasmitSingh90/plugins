1540efdffeb68d824ffac28a3c0f4c56af773ef2
JasmitSingh90 <jasmitsingh90@gmail.com>
2021-08-13 10:11:11 +0530

    Updated the name to video_player_custom
    Updated the name to video_player_custom

--- packages/video_player/video_player/test/video_player_test.dart
+++ packages/video_player/video_player/test/video_player_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:video_player/video_player.dart';
+import 'package:video_player_custom/video_player.dart';
 import 'package:video_player_platform_interface/messages.dart';
 import 'package:video_player_platform_interface/test.dart';
 import 'package:video_player_platform_interface/video_player_platform_interface.dart';
